214ad571d7dbda90c76349d2437fafb3f7e56ecf
Akinwumi sehinde <akinwumisehinde@gmail.com>
2023-03-25 16:52:31 +0100

    last commit

--- src/component/Joke.js
+++ src/component/Joke.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import spinner from '../asset/Book.gif'
 
 const Joke = () => {
+    const [joke,setJoke] = useState({});
+    const [isLoading,setIsLoading] = useState(true);
+    const url = 'https://api.chucknorris.io/jokes/random';
+    // http://api.icndb.com/jokes/random
+
+    const getJoke = () =>{
+        setIsLoading(true)
+        fetch(url)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          console.log(data);
+          setJoke(data);
+          setIsLoading(false)
+        });
+    }
+
+    useEffect(() => {
+     getJoke();
+    }, [])
+    
   return (
     <div className='--flex-center  --100vh'>
         <div className='container'>
             <h1>Jokes Generator</h1>
             <div className='--line'></div>
-            <h4> Joke id: 001</h4>
-            <p> hey oya laugh </p>
-            <button className='--btn --btn-primary'> hGenerate Joke</button>
+            {isLoading ? ( 
+            <div className='--center-all'> <img src={spinner} alt='loading' width={100}/></div>) : (
+                <>
+            <h4> Joke id: {joke.id}</h4>
+            <p> {joke.value}</p>
+            </>
+            )}
+           
+            
+            <button className='--btn --btn-primary' onClick={getJoke}> hGenerate Joke</button>
            
            
             </div>
