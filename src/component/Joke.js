94eab67deffc0577c94d6664f6749cb7622e4c44
Akinwumi sehinde <akinwumisehinde@gmail.com>
2023-03-25 15:39:12 +0100

    Jokes_generator
    Jokes_generator api

---
+++ src/component/Joke.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Joke = () => {
+  return (
+    <div className='--flex-center  --100vh'>
+        <div className='container'>
+            <h1>Jokes Generator</h1>
+            <div className='--line'></div>
+            <h4> Joke id: 001</h4>
+            <p> hey oya laugh </p>
+            <button className='--btn --btn-primary'> hGenerate Joke</button>
+           
+           
+            </div>
+        </div>
+  )
+}
+
+export default Joke
